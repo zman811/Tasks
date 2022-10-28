e55771a1079557bd95bd23aba661e9f7d5b3b349
zman811 <sunburnzman@gmail.com>
2022-10-27 18:19:04 -0600

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+        style={styles.logo}
+      />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
+      <Button title='test' onPress={() => {console.log('test')}}></Button>
+      <TouchableOpacity
+        onPress={() => alert("Hello, world!")}
+        style={{ backgroundColor: "blue" }}
+      >
+        <Text style={{ fontSize: 20, color: "#fff" }}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
   },
 });
